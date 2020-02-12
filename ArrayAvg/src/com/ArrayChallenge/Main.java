f88dcb9b349d0633a9af9316f0d674715a70186e
Somyaranjan Rout <45004977+somyaranjan26@users.noreply.github.com>
2020-02-12 21:58:45 +0530

    Add files via upload

---
+++ ArrayAvg/src/com/ArrayChallenge/Main.java
@@ -0,0 +1,12 @@
+package com.ArrayChallenge;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+	    int[] newInteger = getInteger(5);
+    }
+}
