9a69fedf6839fb5effcb0c8bd155cb3721bc7d8d
Somyaranjan Rout <45004977+somyaranjan26@users.noreply.github.com>
2020-02-20 00:28:37 +0530

    Add files via upload

---
+++ skipOddNumber/src/com/somyaranjan/Main.java
@@ -0,0 +1,41 @@
+package com.somyaranjan;
+
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+
+        char odd[] = {'0', '1', '3', '5', '7', '9'};
+
+        System.out.println("Enter Number: ");
+        String number = sc.next();
+
+
+        System.out.println("Your Input : " + number);
+
+
+//        for(int i=0; i<odd.length; i++) {
+//            System.out.println(odd[i]);
+//        }
+
+//        for( char i=0; i < number.length(); i++) {
+//            for( char j=0; j < odd.length; j++) {
+//
+//                if (number[i] == odd[j]) {
+//                    continue;
+//                }
+//                else {
+//                    System.out.print(number[i]);
+//                }
+//            }
+//       }
+
+
+
+    }
+}
