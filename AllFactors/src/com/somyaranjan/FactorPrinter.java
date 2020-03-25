1721b20cc5347fe1074f70a06dbbfbbff2313b53
Somyaranjan Rout <45004977+somyaranjan26@users.noreply.github.com>
2020-03-26 01:37:49 +0530

    Add files via upload

---
+++ AllFactors/src/com/somyaranjan/FactorPrinter.java
@@ -0,0 +1,15 @@
+package com.somyaranjan;
+
+class FactorPrinter {
+    static void printFactors(int number) {
+        if (number < 1) {
+            System.out.println("Invalid Value");
+        }
+        for (int i=1; i<number; i++) {
+            if(number % i == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
---
+++ AllFactors/src/com/somyaranjan/Main.java
@@ -0,0 +1,8 @@
+package com.somyaranjan;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    FactorPrinter.printFactors(28);
+    }
+}
