006df504c1ff257e1fe03a1895a93ce819385066
Somyaranjan Rout <45004977+somyaranjan26@users.noreply.github.com>
2020-06-13 23:13:17 +0530

    Updated Location

--- EvenDigitSum/Main.java
+++ EvenDigitSum/Main.java
@@ -3,7 +3,7 @@ package com.somyaranjan;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Even Sum : " + EvenDigitSum.getEvenDigitSum(123456789));
+	System.out.println("Even Sum : " + EvenDigitSum.getEvenDigitSum(123456789));
         System.out.println("Even Sum : " + EvenDigitSum.getEvenDigitSum(252));
         System.out.println("Even Sum : " + EvenDigitSum.getEvenDigitSum(-28));
     }
